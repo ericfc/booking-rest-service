36f1b844e21b46ccc0e930056b6a92729b621c5b
Eric Carrillo <ericfc@utexas.edu>
2019-06-21 13:07:57 -0500

    Added database model for Bookings

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    HOUSEKEEPING = 'housekeeping'
+    DOG_WALK = 'dog_walk'
+    BOOKING_TYPE_CHOICES = (
+        (DOG_WALK, 'Dog Walk'), 
+        (HOUSEKEEPING, 'Housekeeping'),
+    )
+
+    email = models.EmailField()
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    booking_type = models.CharField(
+        max_length=30,
+        choices=BOOKING_TYPE_CHOICES,
+    )
+    booking_date = models.DateField(db_index=True)
+    booking_time = models.TimeField(db_index=True)
+
+    def __str__(self):
+        return '{} for {}'.format(self.booking_type, self.name)
+
+    class Meta:
+        ordering = ['booking_date', 'booking_time']
